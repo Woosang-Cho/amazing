be615d77ef99d7789d799aa8229553678c0712a9
Woosang-Cho <woosang05@naver.com>
2025-11-19 21:56:42 +0900

    LPF cpp

--- SMCController/SMCController.cpp
+++ SMCController/SMCController.cpp
@@ -1,22 +1,45 @@
 #include "SMCController.h"
 
+// LPF tau
+// 값이 작을수록 필터링이 강해져 노이즈가 줄지만, 응답 속도가 느려짐
+// 0.01s ~ 0.05s 사이에서 튜닝 고민
+const float TAU = 0.02; 
+
 SMCController::SMCController(float K_, float lambda_, float phi_, float dt_)
-  : K(K_), lambda(lambda_), phi(phi_), dt(dt_), prev_error(0.0) {}
+  : K(K_), lambda(lambda_), phi(phi_), dt(dt_), prev_error(0.0), 
+    current_e_dot(0.0), current_s(0.0) {}
 
 float SMCController::update(float reference, float measured) {
     float e = reference - measured;
-    float e_dot = (e - prev_error) / dt;
+    
+    // 1. 오차 미분값 (e_dot) 계산
+    float raw_e_dot = (e - prev_error) / dt; 
 
-    float s = e + lambda * e_dot;
+    // 2. LPF 계수 (alpha) 계산
+    const float ALPHA = dt / (TAU + dt); 
+    
+    // 3. LPF 적용 (filtered_e_dot = alpha * raw_e_dot + (1 - alpha) * current_e_dot)
+    // current_e_dot은 여기서 이전 필터링 결과(y[k-1])의 역할
+    current_e_dot = ALPHA * raw_e_dot + (1.0 - ALPHA) * current_e_dot; 
+
+    current_s = e + lambda * current_e_dot; 
 
     float sat_s;
-    if (s > phi) sat_s = 1.0;
-    else if (s < -phi) sat_s = -1.0;
-    else sat_s = s / phi;
+    if (current_s > phi) sat_s = 1.0;
+    else if (current_s < -phi) sat_s = -1.0;
+    else sat_s = current_s / phi;
 
     float u = -K * sat_s;
 
     prev_error = e;
 
     return u;
+}
+
+float SMCController::get_e_dot() const {
+    return current_e_dot;
+}
+
+float SMCController::get_s() const {
+    return current_s;
 }
--- SMCController/SMCController.h
+++ SMCController/SMCController.h
@@ -6,14 +6,29 @@
 class SMCController {
   public:
     SMCController(float K_, float lambda_, float phi_, float dt_);
+
     float update(float reference, float measured);
+    
+    // 현재 오차 미분값(e_dot)을 반환
+    float get_e_dot() const; 
+    
+    // 현재 슬라이딩 변수(s)를 반환
+    float get_s() const;     
 
   private:
     float K;
     float lambda;
     float phi;
     float dt;
-    float prev_error;
+    
+    // 이전 오차 (미분값 계산용)
+    float prev_error; 
+    
+    // 현재 계산된 오차 미분값 저장
+    float current_e_dot; 
+    
+    // 현재 계산된 슬라이딩 변수 값 저장
+    float current_s;     
 };
 
 #endif
